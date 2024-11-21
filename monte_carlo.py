8c261265bcfe255df97eefa5adffaa450c838af9
kingoz9000 <mkrs24@student.aau.dk>
2024-11-21 23:16:53 +0100

    initial commit

---
+++ monte_carlo.py
@@ -0,0 +1,24 @@
+from termostat import ThermostatSimple
+from kølerum import Kølerum
+class MonteCarlo:
+    def __init__(self, number_of_simulations, termostat):
+        self.N = number_of_simulations
+        self.termostat = termostat
+        
+    
+    def run_simulation(self):
+        KØL = Kølerum(self.termostat)
+        for _ in range(self.N):
+            KØL.main()
+        temp = KØL.temps
+        electricity = KØL.electicity_cost
+        food_waste = KØL.food_waste
+        return temp, electricity, food_waste
+    
+if __name__ == "__main__":
+    temps, electricity, food_waste = MonteCarlo(8640, ThermostatSimple(5)).run_simulation()
+    #print(f"Temps: {temps}")
+    #print(f"Electricity: {electricity}")
+    #print(f"Food Waste: {food_waste}")
+    print(f"Sum of all costs in Septemper 2022: {sum(electricity)+sum(food_waste)},-")
+        
---
+++ termostat.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+class Thermostat(ABC):
+    pass
+    @abstractmethod
+    def update_compressor(self):
+        pass
+    
+
+
+class ThermostatSimple(Thermostat):
+    def __init__(self, t_target):
+        self.t_target = t_target
+    
+    def update_compressor(self, t_current):
+        return t_current > self.t_target
+
+        
+    
