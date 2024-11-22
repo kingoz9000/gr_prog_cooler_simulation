1bc7164298185cec5720fced52143a6327bfc0df
Mathias Sørensen <mkrs24@student.aau.dk>
2024-11-22 12:12:14 +0100

    More formatting

--- termostat.py
+++ termostat.py
@@ -1,18 +1,17 @@
 from abc import ABC, abstractmethod
+
+
 class Thermostat(ABC):
     pass
+
     @abstractmethod
     def update_compressor(self):
         pass
-    
 
 
 class ThermostatSimple(Thermostat):
     def __init__(self, t_target):
         self.t_target = t_target
-    
+
     def update_compressor(self, t_current):
         return t_current > self.t_target
-
-        
-    
